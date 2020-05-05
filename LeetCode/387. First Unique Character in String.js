3e4f2f8566e52341a3d163410a0d1231b7323b05
Alan Flood <alanwflood.tech@gmail.com>
2020-05-05 15:40:30 +0100

    Update for null strings

--- LeetCode/387. First Unique Character in String.js
+++ LeetCode/387. First Unique Character in String.js
@@ -2,13 +2,17 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var firstUniqChar = function (s) {
   const map = new Map();
+  if (s.length === 0) return -1;
   for (let i = 0; i < s.length; i++) {
     if (map.has(s[i])) map.set(s[i], -1);
     else map.set(s[i], i);
   }
-  return Array.from(map.values()).filter((i) => i > -1)[0];
+  const charIndex = Array.from(map.values()).filter((i) => i > -1)[0];
+  return charIndex !== undefined ? charIndex : -1;
 };
-
-console.log(firstUniqChar("leetcode"));
